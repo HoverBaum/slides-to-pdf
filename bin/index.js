115b0dad401f28bd0069b1705d89e9b94fdbba28
Hendrik <mail@hendrikwallbaum.de>
2019-04-11 22:48:10 +0200

    :sparkles: Cleaner handling of starting point.

--- bin/index.js
+++ bin/index.js
@@ -7,8 +7,7 @@ const packageInfo = require('../package.json')
 program
   .version(packageInfo.version)
   .usage('[options] <baseUrl>')
-  .option('-s, --start-index <index>', 'Start index for slides, defaults to 0 (zero).')
-  .option('-n, --no-starting-index', 'Set this when your starting slide does not have a number. Counting will still be done as if your first slide had a starting index.')
+  .option('-s, --start-index <index>', 'Start index for slides, otherwise stars at \'/\' and next counts to 1.')
   .option('-o, --output <filename>', 'Filename for output PDF defaults to using page title.')
   .parse(process.argv)
 
@@ -20,12 +19,11 @@ if (!baseUrl) {
 }
 
 // Construct options
-const startIndex = program.startIndex || 0
+const startIndex = program.startIndex || ''
 const outputFileName = program.output
 
 const options = {
-  firstSlideNumber: parseInt(startIndex),
-  hasNoFirstNumber: !!program.noStartingIndex
+  firstSlideNumber: startIndex
 }
 if (outputFileName) options.outputFileName = outputFileName
 
